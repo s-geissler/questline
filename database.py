4c00555e0b2650e36c555b44f07d635d86fa18df
Stefan Geissler <info@stefan-geissler.net>
2026-04-02 17:44:52 +0200

    chore: renamed default database to questline

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("QUESTLINE_DATABASE_URL", "sqlite:///./trello.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("QUESTLINE_DATABASE_URL", "sqlite:///./questline.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
